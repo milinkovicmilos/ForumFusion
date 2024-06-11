fec61ee999df68b9c247810eb197c3180b90f249
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2024-06-11 13:07:32 +0200

    Changed log path

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ define("BASE_DIR", dirname(__FILE__, 2));
 define("DOMAIN", "localhost/ForumFusion");
 define("BASE_HOST", "/ForumFusion");
 define("DATA_DIR", BASE_DIR . "/data");
-define("PAGE_ACCESS_LOG_PATH", BASE_DIR . "/admin/data/pageAccessLog.txt");
+define("PAGE_ACCESS_LOG_PATH", BASE_DIR . "/data/admin/pageAccessLog.txt");
 define("LOG_SEPARATOR", ";");
 define("PAGE_ID_SEPARATOR", "-");
 define("DEFAULT_PAGE", "index");
